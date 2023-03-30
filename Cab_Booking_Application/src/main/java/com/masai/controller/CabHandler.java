7667648949b87a3cf92ae0b0083b55b35eda0896
Abhijeet Ojha <avie.just@gmail.com>
2023-03-30 22:51:48 +0530

    Day-3_fw19_0414

--- Cab_Booking_Application/src/main/java/com/masai/controller/CabHandler.java
+++ Cab_Booking_Application/src/main/java/com/masai/controller/CabHandler.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,34 +23,34 @@ public class CabHandler {
 	@Autowired
 	private CabService cabService;
 
-	@PostMapping("/cabAdd")
+	@PostMapping("/cab")
 	public ResponseEntity<Cab> addCab(@RequestBody Cab cab) throws CabException {
 		Cab addedCab = cabService.insertCab(cab);
 		return new ResponseEntity<>(addedCab, HttpStatus.CREATED);
 	}
 
-	@PutMapping("/cab/{id}")
+	@PatchMapping("/cab/{id}")
 	public ResponseEntity<Cab> updateCab(@PathVariable int id, @RequestBody Cab cab) throws CabException {
 		cab.setCabID(id);
 		Cab updatedCab = cabService.updateCab(cab);
 		return new ResponseEntity<>(updatedCab, HttpStatus.OK);
 	}
 
-	@DeleteMapping("/cabdel/{id}")
+	@DeleteMapping("/cab/{id}")
 	public ResponseEntity<Cab> deleteCab(@PathVariable int id) throws CabException {
 		Cab deletedCab = cabService.deleteCab(id);
 		return new ResponseEntity<>(deletedCab, HttpStatus.ACCEPTED);
 	}
 
 	@GetMapping("/cabs")
-	public ResponseEntity<List<Cab>> viewCabsOfType(@RequestParam(name = "type") String cabType) throws CabException {
-		List<Cab> cabs = cabService.viewCabsOfType(cabType);
+	public ResponseEntity<List<Cab>> viewCabsOfType(@RequestParam(name = "type") String carType) throws CabException {
+		List<Cab> cabs = cabService.viewCabsOfType(carType);
 		return new ResponseEntity<>(cabs, HttpStatus.OK);
 	}
 
 	@GetMapping("/cab/count")
-	public ResponseEntity<Integer> countCabsOfType(@RequestParam(name = "type") String cabType) {
-		int count = cabService.countCabsOfType(cabType);
+	public ResponseEntity<Integer> countCabsOfType(@RequestParam(name = "type") String carType) {
+		int count = cabService.countCabsOfType(carType);
 		return new ResponseEntity<>(count, HttpStatus.OK);
 	}
 }
